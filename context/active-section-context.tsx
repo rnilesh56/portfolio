673eb11215a220f55834f88c661b7cdd9674b539
Nilesh Raj <rnilesh56@gmail.com>
2023-11-25 17:36:49 +0530

    Few changes

--- context/active-section-context.tsx
+++ context/active-section-context.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import React, { useState, createContext, useContext } from "react";
 import type { SectionName } from "@/lib/types";
-
+import React, { useState, createContext, useContext } from "react";
 
 type ActiveSectionContextProviderProps = {
   children: React.ReactNode;
 };
+
 type ActiveSectionContextType = {
   activeSection: SectionName;
   setActiveSection: React.Dispatch<React.SetStateAction<SectionName>>;
@@ -21,8 +21,8 @@ export default function ActiveSectionContextProvider({
   children,
 }: ActiveSectionContextProviderProps) {
   const [activeSection, setActiveSection] = useState<SectionName>("Home");
+  const [timeOfLastClick, setTimeOfLastClick] = useState(0); // we need to keep track of this to disable the observer temporarily when user clicks on a link
 
-  const [timeOfLastClick, setTimeOfLastClick] = useState(0);
   return (
     <ActiveSectionContext.Provider
       value={{
@@ -39,10 +39,12 @@ export default function ActiveSectionContextProvider({
 
 export function useActiveSectionContext() {
   const context = useContext(ActiveSectionContext);
+
   if (context === null) {
     throw new Error(
       "useActiveSectionContext must be used within an ActiveSectionContextProvider"
     );
   }
+
   return context;
 }
